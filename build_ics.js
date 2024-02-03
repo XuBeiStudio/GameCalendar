27ae5097933a43158164530360ccd74f65b19c68
liziyi0914 <liziyi0914@gmail.com>
2024-02-03 18:32:43 +0800

    fix(ci): v1.3.1
    修复 build_ics.js

--- build_ics.js
+++ build_ics.js
@@ -1,24 +1,21 @@
 let ics = require('ics');
 let fs = require('fs');
-let months = require('./public/data.json');
+let games = require('./public/data.json');
 
 let events = [];
-for (let i = 0; i < months.length; i++) {
-  let month = months[i];
-  for (let j = 0; j < month.games.length; j++) {
-    let game = month.games[j];
+for (let j = 0; j < games.length; j++) {
+  let game = games[j];
 
-    let { title, releaseDate, platforms } = game;
-    events.push({
-      title: title,
-      description: `《${title}》 现已在 ${platforms.join('、')} 上推出`,
-      start: releaseDate.split('.').map((n) => parseInt(n)),
-      duration: { hours: 24 },
-      url: 'https://game-calendar.liziyi0914.com',
-      organizer: { name: '序碑工作室', email: 'games@xu-bei.cn' },
-      location: platforms.join(', '),
-    });
-  }
+  let { title, releaseDate, platforms } = game;
+  events.push({
+    title: title,
+    description: `《${title}》 现已在 ${platforms.join('、')} 上推出`,
+    start: releaseDate.split('.').map((n) => parseInt(n)),
+    duration: { hours: 24 },
+    url: 'https://game-calendar.liziyi0914.com',
+    organizer: { name: '序碑工作室', email: 'games@xu-bei.cn' },
+    location: platforms.join(', '),
+  });
 }
 
 ics.createEvents(events, (error, value) => {
